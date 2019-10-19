2b988c9fb56937e4e00b3c7cd0b76b703dc986a3
David Tran <david.tran.0924@gmail.com>
2019-10-18 18:06:27 -0700

    Updates

---
+++ precache-manifest.5dafc632d79429e66e89ec87f798c198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a6d97bdc84d1dc0a041f9d803b0749",
+    "url": "/dnd-compendium/index.html"
+  },
+  {
+    "revision": "ddd9eed54762dfb38978",
+    "url": "/dnd-compendium/static/css/main.cd921b30.chunk.css"
+  },
+  {
+    "revision": "ec7abc9031daa0eec76e",
+    "url": "/dnd-compendium/static/js/2.bb8c8cf4.chunk.js"
+  },
+  {
+    "revision": "ddd9eed54762dfb38978",
+    "url": "/dnd-compendium/static/js/main.800351af.chunk.js"
+  },
+  {
+    "revision": "b4414e1e70ba4398be85",
+    "url": "/dnd-compendium/static/js/runtime-main.b1d26bac.js"
+  }
+]);
